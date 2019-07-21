378dea1ee63519b119e7048292932ddda91aabd7
Peter Mandile <peterm1994@hotmail.com>
2019-07-21 19:04:36 +1000

    More info in the inspector
    need to make it pretty

--- src/Inspector/Inspector.ts
+++ src/Inspector/Inspector.ts
@@ -1,8 +1,20 @@
 import {GlobalSystem} from "../ECS/GlobalSystem";
 import {LagomType} from "../ECS/LifecycleObject";
 import {Component} from "../ECS/Component";
-import {observable} from "mobx";
+import {computed, observable} from "mobx";
 import {Entity} from "../ECS/Entity";
+
+class EntityStuff
+{
+    @observable x: number = 0;
+    @observable y: number = 0;
+
+    constructor(x: number, y: number)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 export class Inspector extends GlobalSystem
 {
@@ -13,11 +25,18 @@ export class Inspector extends GlobalSystem
 
     @observable public entities: string[] = [];
     @observable public inspectingEntity: Entity | null = null;
+    @observable public entityStuff: EntityStuff = new EntityStuff(0, 0);
 
     update(delta: number): void
     {
         this.entities = this.getScene().entities.map(
             entity => entity.name);
+
+        if (this.inspectingEntity)
+        {
+            this.entityStuff.x = this.inspectingEntity.transform.x;
+            this.entityStuff.y = this.inspectingEntity.transform.y;
+        }
     }
 
     types(): LagomType<Component>[]
--- src/React/InspectorComponents.tsx
+++ src/React/InspectorComponents.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import {Game} from "../ECS/Game";
 import {Inspector} from "../Inspector/Inspector";
 import {observer} from "mobx-react";
+import {Entity} from "../ECS/Entity";
+import {Log} from "../Common/Util";
+import {Component} from "../ECS/Component";
+import {number} from "prop-types";
 
 @observer
 export class InspectorComponent extends React.Component<{ game: Game }, {}>
@@ -51,9 +55,35 @@ export class EntityInfo extends React.Component<{ inspector: Inspector }, {}>
         const entity = this.props.inspector.inspectingEntity;
         if (entity !== null)
         {
-            return (<ul>{entity.components.map((item, idx) => {
-                return <li key={idx}>{item.constructor.name}</li>
-            })}</ul>)
+            return (<div>{this.basicInfo(entity)}{this.componentList(entity)}</div>)
+        }
+        return null;
+    }
+
+    private componentList(entity: Entity)
+    {
+        return <ul>{entity.components.map((item, idx) => {
+            return <li key={idx}>{item.constructor.name}<pre>{JSON.stringify(item, (key, value) => {
+                if (value instanceof Component)
+                {
+                    return value;
+                }
+                if (!(value instanceof Object))
+                {
+                    return value;
+                }
+                return undefined;
+            }, 4)}</pre></li>
+        })}</ul>;
+    }
+
+    private basicInfo(entity: Entity)
+    {
+        if (this.props.inspector.entityStuff)
+        {
+            return <div>
+                <pre>{JSON.stringify(this.props.inspector.entityStuff, null, 4)}</pre>
+            </div>
         }
         return null;
     }
