180a8e9cac645be8122664f4bba357cb13da4eca
Peter Mandile <peterm1994@hotmail.com>
2019-08-05 23:16:05 +1000

    Added collision events back

--- src/DetectCollisions/DetectActive.ts
+++ src/DetectCollisions/DetectActive.ts
@@ -2,17 +2,12 @@ import {Component} from "../ECS/Component";
 
 export class DetectActive extends Component
 {
+    // Next frame movement
     pendingX = 0;
     pendingY = 0;
 
     dxLastFrame = 0;
     dyLastFrame = 0;
-
-    lastPendingX = 0;
-    lastPendingY = 0;
-
-    lastX: number = 0;
-    lastY: number = 0;
 
     move(x: number, y: number)
     {
--- src/DetectCollisions/DetectCollisions.ts
+++ src/DetectCollisions/DetectCollisions.ts
@@ -36,6 +36,9 @@ export class DetectActiveCollisionSystem extends System
         // New thing. Move incrementally, alternating x/y until we are either at the destination or have hit something.
         this.runOnEntities((entity: Entity, collider: DetectCollider, body: DetectActive) => {
 
+            const collidersLastFrame = collider.collidersLastFrame;
+            collider.collidersLastFrame = [];
+
             // TODO gravity extracted
             body.pendingY = 0.1 * delta;
 
@@ -56,7 +59,7 @@ export class DetectActiveCollisionSystem extends System
                     const potentials = collider.body.potentials();
                     for (let potential of potentials)
                     {
-                        const otherComp = (<any>potential).lagom_component;
+                        const otherComp = (<any>potential).lagom_component as DetectCollider;
                         const result = new Result();
 
                         // Check layers, then do actual collision check
@@ -66,6 +69,9 @@ export class DetectActiveCollisionSystem extends System
                             // Move out of the collision, we are done in this direction.
                             collider.body.x -= result.overlap_x * result.overlap;
                             xMag = 0;
+
+                            DetectActiveCollisionSystem.fireCollisionEvents(collidersLastFrame,
+                                                                            otherComp, collider, result);
                         }
                     }
                     xMag -= dx;
@@ -91,11 +97,17 @@ export class DetectActiveCollisionSystem extends System
                             // Move out of the collision, we are done in this direction.
                             collider.body.y -= result.overlap_y * result.overlap;
                             yMag = 0;
+
+                            DetectActiveCollisionSystem.fireCollisionEvents(collidersLastFrame,
+                                                                            otherComp, collider, result);
                         }
                     }
                     yMag -= dy;
                 }
             }
+
+            // Trigger the exist event for anything that is no longer colliding
+            collidersLastFrame.forEach(val => collider.onCollisionExit.trigger(collider, val));
 
             // Do a final update of the system.
             this.detectSystem.update();
@@ -110,104 +122,33 @@ export class DetectActiveCollisionSystem extends System
             entity.transform.x = collider.body.x - collider.xOff;
             entity.transform.y = collider.body.y - collider.yOff;
         });
+    }
 
-        return;
-
-
-        // TODO we may be able to do this more efficiently?
-        // Step 1, move everything in the engine
-        this.runOnEntities((entity: Entity, collider: DetectCollider, body: DetectActive) => {
-
-            body.pendingY = 0.1 * delta;
-
-            body.lastX = Math.round(collider.body.x - collider.xOff);
-            body.lastY = Math.round(collider.body.y - collider.yOff);
-
-            // TODO We may have desynced from the actual entity pos if something else interfered.
-            collider.body.x += Math.round(body.pendingX);
-            collider.body.y += Math.round(body.pendingY);
-
-            // TODO handle angles as above for rotation changes. can circles rotate? i think so.
-            body.lastPendingX = body.pendingX;
-            body.lastPendingY = body.pendingY;
-
-            body.pendingX = 0;
-            body.pendingY = 0;
-        });
-
-        // Update positions in the engine.
-        this.detectSystem.update();
-
-        // Step 2, detect collisions
-        this.runOnEntities((entity: Entity, collider: DetectCollider, body: DetectActive) => {
-
-            const collidersLastFrame = collider.collidersLastFrame;
-            collider.collidersLastFrame = [];
-
-            const potentials = collider.body.potentials();
-            for (let potential of potentials)
-            {
-                const otherComp = (<any>potential).lagom_component;
-                const result = new Result();
-
-                // Check layers, then do actual collision check
-                if (this.collisionMatrix.canCollide(collider.layer, otherComp.layer)
-                    && collider.body.collides(potential, result))
-                {
-                    this.resolveCollision(collider, body, {other: otherComp, result: result});
-                    // Continuous collision
-                    if (collidersLastFrame.includes(otherComp))
-                    {
-                        collider.onCollision.trigger(collider, {other: otherComp, result: result});
-                    }
-                    else
-                    {
-                        // New collision
-                        collider.onCollisionEnter.trigger(collider, {other: otherComp, result: result});
-                    }
-                    Util.remove(collidersLastFrame, otherComp);
-                    collider.collidersLastFrame.push(otherComp);
-                }
-            }
-
-            // Trigger the exist event for anything that is no longer colliding
-            collidersLastFrame.forEach(val => collider.onCollisionExit.trigger(collider, val));
-        });
-
-        // Step 4, move entities to resolved locations
-        this.runOnEntities((entity: Entity, collider: DetectCollider) => {
-            entity.transform.x = collider.body.x - collider.xOff;
-            entity.transform.y = collider.body.y - collider.yOff;
-        });
-
-        // Update again with final positions.
-        this.detectSystem.update();
+    private static fireCollisionEvents(collidersLastFrame: DetectCollider[], otherComp: DetectCollider,
+                                       collider: DetectCollider, result: Result)
+    {
+        if (collidersLastFrame.includes(otherComp))
+        {
+            // Continuous collision
+            collider.onCollision.trigger(collider, {other: otherComp, result: result});
+        }
+        else
+        {
+            // New collision
+            collider.onCollisionEnter.trigger(collider, {other: otherComp, result: result});
+        }
+        Util.remove(collidersLastFrame, otherComp);
+        collider.collidersLastFrame.push(otherComp);
     }
 
     addBody(body: DetectCollider)
     {
         this.detectSystem.insert(body.body);
-        // body.onCollision.register(this.resolveCollision.bind(this));
     }
 
     removeBody(body: DetectCollider)
     {
         this.detectSystem.remove(body.body);
-        // body.onCollision.deregister(this.resolveCollision.bind(this));
-    }
-
-    resolveCollision(caller: DetectCollider, body: DetectActive, data: { other: DetectCollider, result: Result })
-    {
-        // Step 3, resolve collisions
-        caller.body.x -= data.result.overlap_x * data.result.overlap;
-        caller.body.y -= data.result.overlap_y * data.result.overlap;
-
-
-        // TODO retry? do x and y independently, make sure it doesnt try again for ever though
-        // in the line above, only subtract in the smaller direction, then do the coll detection again for the other
-        // movement. inline it, i don't want to affect the rest of the run if possible...... this is kinda crazy and
-        // possibly wrong.
-        this.detectSystem.update();
     }
 
     update(delta: number): void
