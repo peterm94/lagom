46a01d38468a250a33b06dee9a3ffb4125d9339f
Peter Mandile <peterm1994@hotmail.com>
2019-10-05 12:12:02 +1000

    Added template game

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,14 @@ import * as ReactDOM from "react-dom";
 import {LagomGameComponent} from "./React/LagomGameComponent";
 import {InspectorComponent} from "./React/InspectorComponents";
 import {Downshaft} from "./examples/Downshaft/Downshaft";
+import {HexGame} from "./examples/LD45/HexGame";
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-const game = new Downshaft();
+const game = new HexGame();
 
 ReactDOM.render(
     <div>
