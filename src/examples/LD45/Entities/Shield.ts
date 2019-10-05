4c4da13e58a15dfed5f42d028d476de2d33f4bda
Dylan Jenkins <dylanljenkins@gmail.com>
2019-10-05 22:29:48 +1000

    Updated the shield.

--- src/examples/LD45/Entities/Shield.ts
+++ src/examples/LD45/Entities/Shield.ts
@@ -4,6 +4,7 @@ import {Hex} from "../Hexagons/Hex";
 import shieldSpr from '../art/shield.png'
 import {SpriteSheet} from "../../../Common/Sprite/SpriteSheet";
 import {Sprite} from "../../../Common/Sprite/Sprite";
+import {AnimatedSprite, AnimationEnd} from "../../../Common/Sprite/AnimatedSprite";
 
 const shieldSheet = new SpriteSheet(shieldSpr, 32, 32);
 
@@ -17,6 +18,11 @@ export class ShieldHex extends HexEntity
     onAdded()
     {
         super.onAdded();
+
+        this.addComponent(new AnimatedSprite(
+            shieldSheet.textures([[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]),
+            {xAnchor: 0.5, yAnchor: 0.5, animationEndAction: AnimationEnd.LOOP, animationSpeed: 125}));
+
         this.addComponent(new Sprite(shieldSheet.texture(0, 0), {xAnchor: 0.5, yAnchor: 0.5}));
     }
 }
